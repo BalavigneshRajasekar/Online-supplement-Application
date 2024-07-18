be170bd811ea5167b6544cd1ef01c277b9ba6088
BalavigneshRajasekar <you@example.com>
2024-07-18 20:00:18 +0530

    mongo atlas and .env added

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 //Middleware Goes here
@@ -11,6 +13,9 @@ app.use(express.static("./public"));
 //Routes Goes here
 
 //Server starts listening
-app.listen("3000", () => {
-  console.log("Server started on port 3000"); // Server is running on port 3000
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log("Connected to MongoDB"); // Connected to MongoDB
+  app.listen("3000", () => {
+    console.log("Server started on port 3000"); // Server is running on port 3000
+  });
 });
