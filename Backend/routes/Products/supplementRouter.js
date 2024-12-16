0a447d32314935be21cae75abb82e32b44d76bc9
BalavigneshRajasekar <you@example.com>
2024-12-16 20:20:51 +0530

    Edit and delete added

--- Backend/routes/Products/supplementRouter.js
+++ Backend/routes/Products/supplementRouter.js
@@ -16,7 +16,6 @@ supplementRouter.get("/products1", async (req, res) => {
     const supplements = await Products.find({});
     res.json(supplements);
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: "Server Error" });
   }
 });
@@ -81,13 +80,89 @@ supplementRouter.post("/add", upload.array("image"), async (req, res) => {
     });
 
     const savedSupplement = await newSupplement.save();
-    console.log(savedSupplement);
 
     res
       .status(201)
       .json({ message: "saved successfully", data: savedSupplement });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ message: "Server Error", err: error.message });
+  }
+});
+
+// Sample route for updating a supplement by ID
+
+supplementRouter.put("/update/:id", upload.array("image"), async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    quantity,
+    category,
+    supplementType,
+    expirationDate,
+  } = req.body;
+
+  const id = req.params;
+
+  try {
+    //Upload image to cloudinary
+    const mediaUrls = await Promise.all(
+      req.files.map(async (file) => {
+        return new Promise((resolve, reject) => {
+          const uploadStream = cloudinary.uploader.upload_stream(
+            {
+              resource_type: "auto",
+              upload_preset: "Unsigned",
+            },
+            (error, result) => {
+              if (error) return reject(error);
+              resolve(result.secure_url);
+            }
+          );
+          uploadStream.end(file.buffer);
+        });
+      })
+    );
+
+    const newSupplement = await Products.findByIdAndUpdate(
+      id,
+      {
+        name,
+        price,
+        description,
+        quantity,
+        image: mediaUrls.filter(
+          (file) => file.endsWith(".jpg") || file.endsWith(".png")
+        ),
+        category,
+        supplementType,
+        expirationDate,
+      },
+      { new: true, runValidators: true }
+    );
+
+    res
+      .status(201)
+      .json({ message: "Updated successfully", data: newSupplement });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error", err: error.message });
+  }
+});
+
+// Sample route for deleting a supplement by ID
+
+supplementRouter.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const deletedSupplement = await Products.findByIdAndDelete(id);
+    if (!deletedSupplement)
+      return res.status(404).json({ message: "Supplement not found" });
+
+    res.json({ message: "Deleted successfully", data: deletedSupplement });
+  } catch (error) {
     res.status(500).json({ message: "Server Error", err: error.message });
   }
 });
