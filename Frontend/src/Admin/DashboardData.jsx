2c1fa5d8a6282fc7db9e1533501722be7cad2a00
BalavigneshRajasekar <you@example.com>
2025-01-21 21:57:11 +0530

    Dashboard data component created

---
+++ Frontend/src/Admin/DashboardData.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function DashboardData() {
+  return (
+    <div>
+      <h1>Dashboard data will be shown here</h1>
+    </div>
+  );
+}
+
+export default DashboardData;
