2ea656c56d182459bfd457b2699031121a71e31a
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-02-09 15:11:10 +0530

    Endpoint to add courier details created

--- Backend/routes/Products/orders.js
+++ Backend/routes/Products/orders.js
@@ -81,4 +81,27 @@ orderRouter.put(
   }
 );
 
+//Route to update Courier Information and tracking ID
+
+orderRouter.put(
+  "/update/courier/:id",
+  // loginAuth,
+  // roleAuth("Admin"),
+  async (req, res) => {
+    const { id } = req.params;
+    const { courier, trackingId } = req.body;
+     
+    try {
+      const order = await Orders.findOneAndUpdate({_id:id},{$set:{"paymentData.shipping.carrier":courier,"paymentData.shipping.tracking_number":trackingId}},{new:true})
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+      res.json({ message: `Courier and tracking ID updated`, data: order });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: "Server Error", e });
+    }
+  }
+);
+
 module.exports = orderRouter;
