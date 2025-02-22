6dcefee728f0ca2fe25e66142e3a6cd0c5a8f81e
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-02-23 00:48:46 +0530

    New env added

--- Backend/routes/Authentication/googleLogin.js
+++ Backend/routes/Authentication/googleLogin.js
@@ -4,10 +4,22 @@ const jwt = require("jsonwebtoken");
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-// Parse JSON stored in environment variable
-// const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
-// console.log(serviceAccount)
-const service = require('../../firebaseCred.json')
+// Firebase Configuration object 
+const service ={
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key:process.env.private_key ,
+  client_email:process.env.client_email ,
+  client_id:process.env.client_id ,
+  auth_uri:process.env.auth_uri ,
+  token_uri:process.env.token_uri ,
+  auth_provider_x509_cert_url:process.env.auth_provider_x509_cert_url ,
+  client_x509_cert_url:process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+}
+
+
 admin.initializeApp({
   credential: admin.credential.cert(service),
 });
